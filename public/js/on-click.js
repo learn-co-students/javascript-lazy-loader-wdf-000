739c1823f74417c31a1f74f87a0efde170c8606d
Katie Hoffman <ktahoffman@gmail.com>
2015-04-29 18:52:28 -0400

    remove passing code

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -1,7 +1,6 @@
 "use strict";
 
 $(document).ready(function() {
-  $("#load-cars").on("click", function() {
-    fetchJSON();
-  });
+  // add click listener here
+  // it should call on fetchJSON()
 });
